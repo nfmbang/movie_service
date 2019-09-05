11f24356b4c5d224bbe59f75e17d2f7dc15f935d
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-09-05 16:59:18 +0200

    init

---
+++ src/test/java/utils/SettingsTest.java
@@ -0,0 +1,33 @@
+package utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class SettingsTest {
+
+    @Disabled
+    @Test
+    public void testGetSingleProperty() {
+        assertEquals("3307", Settings.getPropertyValue("db.port"));
+    }
+
+    @Disabled
+    @Test
+    public void testGetDEV_DBConnection() {
+        //This test is a little bit hacky since it uses a property that can be changed by users
+        //Is Better than NO test
+        String dbName = Settings.getPropertyValue("db.database");
+        assertEquals(String.format("jdbc:mysql://localhost:3307/%s", dbName), Settings.getDEV_DBConnection());
+    }
+
+    @Disabled
+    @Test
+    public void testGetTEST_DBConnection() {
+        //This test is also a little bit hacky since it uses a property that can be changed by users
+        //Is Better than NO test
+        String dbName = Settings.getPropertyValue("dbtest.database");
+        assertEquals(String.format("jdbc:mysql://localhost:3307/%s", dbName), Settings.getTEST_DBConnection());
+    }
+
+}
